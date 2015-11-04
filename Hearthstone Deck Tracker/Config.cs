a17042768f1c0051fad682376b6a42fb91768584
ChuckLu <244657538@qq.com>
2015-11-04 19:00:26 +0800

    默认显示场攻，默认不显示抽牌概率

--- Hearthstone Deck Tracker/Config.cs
+++ Hearthstone Deck Tracker/Config.cs
@@ -322,8 +322,8 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(false)]
 		public bool HideDecksInOverlay = false;
 
-		[DefaultValue(false)]
-		public bool HideDrawChances = false;
+		[DefaultValue(true)]
+		public bool HideDrawChances = true;
 
 		[DefaultValue(false)]
 		public bool HideInBackground = false;
@@ -331,8 +331,8 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(false)]
 		public bool HideInMenu = false;
 
-		[DefaultValue(true)]
-		public bool HideOpponentAttackIcon = true;
+		[DefaultValue(false)]
+		public bool HideOpponentAttackIcon = false;
 
 		[DefaultValue(false)]
 		public bool HideOpponentCardAge = false;
@@ -346,8 +346,8 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(false)]
 		public bool HideOpponentCards = false;
 
-		[DefaultValue(false)]
-		public bool HideOpponentDrawChances = false;
+		[DefaultValue(true)]
+		public bool HideOpponentDrawChances = true;
 
 		[DefaultValue(false)]
 		public bool HideOpponentFatigueCount = false;
@@ -358,8 +358,8 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(false)]
 		public bool HideOverlayInSpectator = false;
 
-		[DefaultValue(true)]
-		public bool HidePlayerAttackIcon = true;
+		[DefaultValue(false)]
+		public bool HidePlayerAttackIcon = false;
 
 		[DefaultValue(false)]
 		public bool HidePlayerCardCount = false;
